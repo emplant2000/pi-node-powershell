510bb6a126b03bf77453fd761cb5aacf49039f49
emplant2000 <emplant2000@gmail.com>
2022-05-21 17:11:47 +0900

    Update pi-node.ps1

--- pi-node.ps1
+++ pi-node.ps1
@@ -14,6 +14,11 @@ netsh -f pinode.netsh
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 
+# install docker-desktop
+
+choco install docker-desktop -y
+
+
 # install pinode
 
 choco install pinode -y
