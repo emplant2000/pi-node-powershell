e34bd5d1a64b949c7611e7051dc589b8f02416ca
emplant2000 <emplant2000@gmail.com>
2022-05-25 10:41:05 +0900

    Update pi-node.ps1

--- pi-node.ps1
+++ pi-node.ps1
@@ -10,6 +10,7 @@
 
 netsh -f pinode.netsh
 
+# advfirewall firewall add rule name="Pi Network Docker CMD" dir=in action=allow protocol=TCP localport=31400-31409 enable=yes profile=any >nul
 
 # Enviroment
 
